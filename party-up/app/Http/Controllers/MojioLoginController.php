744ea8a07a73f2752eda23cb8ab519cc2b83a1fd
Duncan Andrew <duncanandrew@live.com>
2017-03-19 03:38:39 -0700

    added redirect to groups

--- party-up/app/Http/Controllers/MojioLoginController.php
+++ party-up/app/Http/Controllers/MojioLoginController.php
@@ -12,8 +12,7 @@ class MojioLoginController extends Controller
     public function test(Request $request, $access_token, $expires_in){
         $request->session()->put('access_token', $access_token);
         $request->session()->put('expire_time', Carbon::now()->addSeconds($expires_in));
-        
-        echo($request->session()->get('expire_time')->toTimeString());
-        //return Redirect::to('/groups');
+
+        return Redirect::to('/groups');
     }
 }
