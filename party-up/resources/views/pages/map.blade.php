047457e44d3c91eedf8d9c3815cc809367a9e1a9
Abhishek Joshi <joshi.abhishek@hotmail.com>
2017-03-19 05:00:10 -0700

    Update

--- party-up/resources/views/pages/map.blade.php
+++ party-up/resources/views/pages/map.blade.php
@@ -155,31 +155,21 @@
             addOtherDriverMarkers();
         }
 
-        function addOtherDriverMarkers() {
+       function addOtherDriverMarkers() {
 
             var i;
             for (i = 0; i < otherDrivers.length; i++) {
                 var way = otherDrivers[i];
 
-                addDriverMarkers(way, 'Uluru2', '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-                    'sandstone rock formation in the southern part of the ' +
-                    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
-                    'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-                    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-                    'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-                    'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-                    'Aboriginal people of the area. It has many springs, waterholes, ' +
-                    'rock caves and ancient paintings. Uluru is listed as a World ' +
-                    'Heritage Site.</p>' +
-                    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-                    'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
-                    '(last visited June 22, 2009).</p>', 'other');
+                addDriverMarkers(way, 'Other Driver Name', '<p>Driver Content</p>', 'other');
             }
         }
 
         function addCurrentDriver() {
-            var curr = { lat: 61.878, lng: -87.659 }
-            currentDriver = curr;
+            if(currentDriver == null) {
+                var curr = { lat: 61.878, lng: -87.659 }
+                currentDriver = curr;
+            } 
 
             addCurrentDriverMarker();
         }
