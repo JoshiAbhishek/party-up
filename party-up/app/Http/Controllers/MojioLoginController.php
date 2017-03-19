85ec6706aba7cf586c195d09f3d5e9dd54ff95e4
Duncan Andrew <duncanandrew@live.com>
2017-03-19 03:33:50 -0700

    get expire time

--- party-up/app/Http/Controllers/MojioLoginController.php
+++ party-up/app/Http/Controllers/MojioLoginController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests;
 use Illuminate\Http\Request;
+use Redirect;
 
 class MojioLoginController extends Controller
 {
-    public function test($access_token, $expires_in){
+    public function test(Request $request, $access_token, $expires_in){
         dd($access_token);
         dd($expires_in);
+        $request->session()->put('access_token', $access_token);
+        $request->session()->put('expire_time', Carbon::now()->addSeconds($expires_in));
+        
+        echo($request->session()->get('expire_time')->toTimeString());
+        //return Redirect::to('/groups');
     }
 }
