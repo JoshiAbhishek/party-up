0c2e6ed7dd1b98b8bae85e47c9ddc2a629b65456
Duncan Andrew <duncanandrew@live.com>
2017-03-19 08:11:59 -0700

    set timeout back to its regular value

--- party-up/app/Http/Controllers/MojioLoginController.php
+++ party-up/app/Http/Controllers/MojioLoginController.php
@@ -11,7 +11,7 @@ class MojioLoginController extends Controller
 {
     public function setSession(Request $request, $access_token, $expires_in){
         $request->session()->put('access_token', $access_token);
-        $request->session()->put('expire_time', Carbon::now()->addSeconds(6));
+        $request->session()->put('expire_time', Carbon::now()->addSeconds($expires_in));
 
         return Redirect::to('/Groups');
     }
