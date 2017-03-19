ec4a277a4317a769871308dbce3fb752c32d5539
Abhishek Joshi <joshi.abhishek@hotmail.com>
2017-03-19 04:55:29 -0700

    Added PNG markers

--- party-up/resources/views/pages/map.blade.php
+++ party-up/resources/views/pages/map.blade.php
@@ -98,7 +98,7 @@
                 map.fitBounds(bounds);
             }   
         }
-        
+
         function updateCenter() {
              if ((!map.getBounds().contains(currentDriver))) {
                 //map.setCenter(marker.getPosition());  
@@ -185,19 +185,7 @@
         }
 
         function addCurrentDriverMarker() {
-            addDriverMarkers({ lat: 61.878, lng: -87.659 }, 'Uluru', '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-                'sandstone rock formation in the southern part of the ' +
-                'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
-                'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-                '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-                'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-                'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-                'Aboriginal people of the area. It has many springs, waterholes, ' +
-                'rock caves and ancient paintings. Uluru is listed as a World ' +
-                'Heritage Site.</p>' +
-                '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-                'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
-                '(last visited June 22, 2009).</p>', 'driver');
+            addDriverMarkers(currentDriver, 'Driver Name', '<p>Driver Content</p>', 'driver');
 
 
             map.setCenter(new google.maps.LagLng(currentDriver));
@@ -246,10 +234,10 @@
             var i;
 
             if (type == 'driver') {
-                i = 'marker1.png';
+                i = '/public/images/marker1.png';
             }
             else {
-                i = 'marker2.png';
+                i = '/public/images/marker2.png';
             }
 
             var marker = new google.maps.Marker({
