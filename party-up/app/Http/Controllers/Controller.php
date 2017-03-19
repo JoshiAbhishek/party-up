8eb38ac4dff6cafdd89bb13d9dfb0d2c391c7adf
Duncan Andrew <duncanandrew@live.com>
2017-03-19 07:50:18 -0700

    fixed redirecting and removed test code

--- party-up/app/Http/Controllers/Controller.php
+++ party-up/app/Http/Controllers/Controller.php
@@ -17,12 +17,10 @@ class Controller extends BaseController
 
     public function checkLoginStatus(Request $request){
         if(!$request->session()->has('access_token')){
-            echo("Redirect");
             return Redirect::to('/signin')->send();
         }else if(Carbon::now()->gt($request->session()->get('expire_time'))){
             $request->session()->forget('access_token');
             $request->session()->forget('expire_time');
-            echo("Redirect");
             return Redirect::to('/signin')->send();
         }else{
             // Authenticated
